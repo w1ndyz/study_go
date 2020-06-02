49f6ef047d58a24d500d0546394fa8abef580784
ww <ww@at.local>
2020-06-02 22:35:20 +0700

    新增3.5

---
+++ section3/3.10/printints.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	comma("12345")
+}
+
+func comma(s string) {
+	var buf bytes.Buffer
+
+	l := len(s)
+	mod := l % 3
+	if mod > 0 {
+		buf.Write([]bytes(s[:mod] + ","))
+	}
+	for mod+3 < l {
+		buf.Write([]bytes(s[mod:mod+3] + ","))
+		mod += 3
+	}
+	if mod+3 == l {
+		buf.Write([]bytes(s[mod : mod+3]))
+	}
+	fmt.Println(buf.String())
+}
---
+++ section3/3.5/mandelbrot.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"io"
+	"log"
+	"math/cmplx"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		cmpli(w)
+	})
+	log.Fatal(http.ListenAndServe("localhost:8001", nil))
+}
+
+func cmpli(out io.Writer) {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < width; py++ {
+		y := float64(py)/width*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+	png.Encode(out, img)
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			v := 255 - contrast*n
+			return color.YCbCr{v, 255 - v, 255}
+		}
+	}
+	return color.Black
+}
