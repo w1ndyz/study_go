9129edec24f8fc3d4be06f49eadaff171215bf75
ww <ww@at.local>
2020-06-13 20:22:15 +0700

    .

---
+++ section8/8.5/thumbnail.go
@@ -0,0 +1,13 @@
+package main
+
+import "log"
+
+func ImageFile(infile string) (string, error) {}
+
+func makeThumbnails(filenames []string) {
+	for _, f := range filenames {
+		if _, err := ImageFile(f); err != nil {
+			log.Println(err)
+		}
+	}
+}
